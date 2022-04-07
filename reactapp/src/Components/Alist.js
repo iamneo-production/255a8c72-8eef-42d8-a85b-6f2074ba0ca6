3e1e61f6acf4ecf00e640ef6fd209d1d27f98691
Kommuri-Swetha <19501a0599@pvpsiddhartha.ac.in>
2022-04-07 14:18:32 +0000

    list

---
+++ reactapp/src/Components/Alist.js
@@ -0,0 +1,45 @@
+import './Alist.css';
+import React from "react";
+function Alist() {
+return (
+  <div>
+    <h1 align="center" id="head"> Appointment List</h1>
+	<div className="App">
+	<table>
+  	
+		<tr id="t1">
+      <td> </td>
+		<td>User 7</td>
+		
+		<td>18-04-2021</td>
+    <td>04:00 PM</td>
+    <td> </td>
+    <td> </td>
+		</tr>
+		<tr id="t1">
+    <td> </td>
+		<td>User 8</td>
+		
+		<td>22-04-2021</td>
+    <td>11:30 AM</td>
+    <td> </td>
+    <td> </td>
+		</tr>
+		<tr id="t1">
+    <td> </td>
+		<td>User 9</td>
+		
+		<td>21-04-2021</td>
+    <td>03:00 PM</td>
+    <td> </td>
+    <td> </td>
+		</tr>
+	</table>
+	</div>
+  </div>
+);
+}
+
+export default Alist;
+
+
